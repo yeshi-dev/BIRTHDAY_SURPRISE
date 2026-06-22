099da27d943e0e0fcc73ebe9a0d19569f58414a9
yeshi-dev <liyanabarock@gmail.com>
2026-06-22 02:52:05 -0900

    birthday website

---
+++ script.js
@@ -0,0 +1,6 @@
+function showSurprise(){
+  document.getElementById("surpriseMessage").
+  innerHTML="HAPPY BIRTHDAY <br></br>"+"you are an inspiration to our family <br><br>"+
+  "made with love you by your sister";
+  document.getElementById("brothersMusic").play();
+}
